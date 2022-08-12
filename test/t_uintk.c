e274bd284eac93f9d3b4bfc62d171b5976ca8fd8
Li xilin <lixilin@gmx.com>
2022-08-12 12:29:11 +0800

    test: failed to build

--- test/t_uintk.c
+++ test/t_uintk.c
@@ -20,7 +20,7 @@
  * THE SOFTWARE.
  */
 
-#include "ax/uintk.h"
+#include "ax/u1024.h"
 #include "ut/runner.h"
 #include "ut/suite.h"
 
@@ -30,21 +30,21 @@
 #include <stdio.h>
 #include <string.h>
 
-void factorial(ax_uintk* n, ax_uintk* res)
+void factorial(ax_u1024* n, ax_u1024* res)
 {
-	ax_uintk tmp;
-	ax_uintk_assign(&tmp, n);
+	ax_u1024 tmp;
+	ax_u1024_assign(&tmp, n);
 
-	ax_uintk_dec(n);
+	ax_u1024_dec(n);
 
-	while (!ax_uintk_is_zero(n)) {
-		ax_uintk_mul(&tmp, n, res);
-		ax_uintk_dec(n);
+	while (!ax_u1024_is_zero(n)) {
+		ax_u1024_mul(&tmp, n, res);
+		ax_u1024_dec(n);
 
-		ax_uintk_assign(&tmp, res);
+		ax_u1024_assign(&tmp, res);
 	}
 
-	ax_uintk_assign(res, &tmp);
+	ax_u1024_assign(res, &tmp);
 }
 
 
@@ -56,18 +56,18 @@ static void test_factorial(ut_runner *r)
 		"1b30964ec395dc24069528d54bbda40d16e966ef9a70eb21b5b294"
 		"3a321cdf10391745570cca9420c6ecb3b72ed2ee8b02ea2735c61a"
 		"000000000000000000000000";
-	ax_uintk num = { 0 };
-	ax_uintk res = { 0 };
-	ax_uintk_from_int(&num, 100);
+	ax_u1024 num = { 0 };
+	ax_u1024 res = { 0 };
+	ax_u1024_from_int(&num, 100);
 	factorial(&num, &res);
-	ax_uintk_to_string(&res, resbuf, sizeof resbuf);
+	ax_u1024_to_string(&res, resbuf, sizeof resbuf);
 	ut_assert(r, strcmp(expect, resbuf) == 0);
 
 
 }
-ut_suite *suite_for_uintk()
+ut_suite *suite_for_u1024()
 {
-	ut_suite* suite = ut_suite_create("uintk");
+	ut_suite* suite = ut_suite_create("u1024");
 
 	ut_suite_add(suite, test_factorial, 0);
 
