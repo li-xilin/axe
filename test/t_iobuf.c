db5bc0fe899ad1fdfbf7a211a0c6c4f27efff7db
Li Xilin <lixilin@gmx.com>
2024-04-27 15:08:43 +0800

    fix: name of function ax_iobuf_flatten has changed

--- test/t_iobuf.c
+++ test/t_iobuf.c
@@ -28,7 +28,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static void flatten(ut_runner *r)
+static void pullup(ut_runner *r)
 {
 	{
 		char buffer[16];
@@ -36,7 +36,7 @@ static void flatten(ut_runner *r)
 		ax_iobuf_init(&b, buffer,sizeof buffer);
 		ax_iobuf_write(&b, "hello", 5);
 		ax_iobuf_write(&b, " world", 6);
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "hello world";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 	}
@@ -44,7 +44,7 @@ static void flatten(ut_runner *r)
 	{
 		char buffer[] = " abcdefghij                                          ";
 		ax_iobuf b = { .buf = (uint8_t *)buffer, .front = 1, .rear = 11, .size = 53 };
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "abcdefghij";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 	}
@@ -52,7 +52,7 @@ static void flatten(ut_runner *r)
 	{
 		char buffer[] = "                                           abcdefghij";
 		ax_iobuf b = { .buf = (uint8_t *)buffer, .front = 43, .rear = 0, .size = 53 };
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "abcdefghij";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 	}
@@ -60,7 +60,7 @@ static void flatten(ut_runner *r)
 	{
 		char buffer[] = "klmnop                                     abcdefghij";
 		ax_iobuf b = { .buf = (uint8_t *)buffer, .front = 43, .rear = 6, .size = 53 };
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "abcdefghijklmnop";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 	}
@@ -68,7 +68,7 @@ static void flatten(ut_runner *r)
 	{
 		char buffer[] = "klmnopqrstuvwxyz0123456789abcdefjhijklmn   abcdefghij";
 		ax_iobuf b = { .buf = (uint8_t *)buffer, .front = 43, .rear = 40, .size = 53 };
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "abcdefghijklmnopqrstuvwxyz0123456789abcdefjhijklmn";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 	}
@@ -76,7 +76,7 @@ static void flatten(ut_runner *r)
 	{
 		char buffer[] = "efjhijklmn   abcdefghijklmnopqrstuvwxyz0123456789abcd";
 		ax_iobuf b = { .buf = (uint8_t *)buffer, .front = 13, .rear = 10, .size = 53 };
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "abcdefghijklmnopqrstuvwxyz0123456789abcdefjhijklmn";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 	}
@@ -84,7 +84,7 @@ static void flatten(ut_runner *r)
 	{
 		char buffer[] = "klmnopqrstuvwxyz0123456789abcdefjhijklmnop abcdefghij";
 		ax_iobuf b = { .buf = (uint8_t *)buffer, .front = 43, .rear = 42, .size = 53 };
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "abcdefghijklmnopqrstuvwxyz0123456789abcdefjhijklmnop";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 	}
@@ -93,7 +93,7 @@ static void flatten(ut_runner *r)
 	{
 		char buffer[] = "efjhijklmnop abcdefghijklmnopqrstuvwxyz0123456789abcd";
 		ax_iobuf b = { .buf = (uint8_t *)buffer, .front = 13, .rear = 12, .size = 53 };
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "abcdefghijklmnopqrstuvwxyz0123456789abcdefjhijklmnop";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 	}
@@ -101,7 +101,7 @@ static void flatten(ut_runner *r)
 	{
 		char buffer[] = "klmnopqrstuvwxyz0123456789                 abcdefghij";
 		ax_iobuf b = { .buf = (uint8_t *)buffer, .front = 43, .rear = 26, .size = 53 };
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "abcdefghijklmnopqrstuvwxyz0123456789";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 	}
@@ -109,7 +109,7 @@ static void flatten(ut_runner *r)
 	{
 		char buffer[] = "efjhijklmnop                           0123456789abcd";
 		ax_iobuf b = { .buf = (uint8_t *)buffer, .front = 39, .rear = 12, .size = 53 };
-		char *act = ax_iobuf_flatten(&b);
+		char *act = ax_iobuf_pullup(&b);
 		char *exp = "0123456789abcdefjhijklmnop";
 		ut_assert_mem_equal(r, exp, strlen(exp), act, ax_iobuf_data_size(&b));
 
@@ -122,7 +122,7 @@ static void flatten(ut_runner *r)
 ut_suite *suite_for_iobuf()
 {
 	ut_suite* suite = ut_suite_create("iobuf");
-	ut_suite_add(suite, flatten, 0);
+	ut_suite_add(suite, pullup, 0);
 	return suite;
 }
 
